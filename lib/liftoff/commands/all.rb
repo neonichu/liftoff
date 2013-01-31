7a0c467498919f21d433291530bd568dfff4faa4
Boris Bügling <boris@icculus.org>
2013-01-31 23:30:31 +0100

    Added all my commands to the "all" command.

--- lib/liftoff/commands/all.rb
+++ lib/liftoff/commands/all.rb
@@ -8,13 +8,20 @@ command :all do |c|
       exit
     end
 
-    GitHelper.new.generate_files
-
     xcode_helper = XcodeprojHelper.new
-    xcode_helper.set_indentation_level 4
     xcode_helper.treat_warnings_as_errors
     xcode_helper.add_todo_script_phase
     xcode_helper.enable_hosey_warnings
     xcode_helper.enable_static_analyzer
+
+    xcode_helper.handle_default_images
+    xcode_helper.move_supporting_files_group
+    xcode_helper.remove_info_plist_strings
+    xcode_helper.sort_all_groups
+    xcode_helper.set_deployment_target('5.1')
+
+    git_helper = GitHelper.new
+    git_helper.generate_files
+    git_helper.stage_default_changes
   end
 end
